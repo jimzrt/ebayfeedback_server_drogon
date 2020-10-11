259a961c5013e24558ac38bea058bc89b1d7087f
jimzrt <james.tophoven@gmail.com>
2020-10-11 17:29:41 +0200

    add docker-compose.yml disable logging for now

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # drogon ebayfeedback server
 ```
+git clone https://github.com/jimzrt/ebayfeedback_server_drogon.git
+cd ebayfeedback_server_drogon
 git submodule update --init --recursive
 ```
 ```
--- main.cc
+++ main.cc
@@ -8,6 +8,7 @@ int main()
     //Load config file
     drogon::app().loadConfigFile("../config.json");
     //Run HTTP framework,the method will block in the internal event loop
+    std::cout << "Starting ebayfeedback server" << std::endl;
     drogon::app().run();
     return 0;
 }
